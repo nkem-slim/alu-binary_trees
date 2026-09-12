4b354029916d377687b4f8b54df749f640aea1ae
Nkem Vincent Nweke <n.nweke@alustudent.com>
2026-09-12 18:31:44 +0200

    fix: resolve betty coding standards and other standards required

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,12 +2,18 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
+/**
+ * binary_tree_node - creates a new binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * 
+ * Return: pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
     binary_tree_t *temp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    if(temp == NULL)
-    {
+    if (temp == NULL) {
         printf("Memory allocation failed\n");
         return NULL;
     }
