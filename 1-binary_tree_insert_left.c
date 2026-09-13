3b36c4f656202786c85b5cfaaf3b4d5f19cd646b
Nkem Vincent Nweke <n.nweke@alustudent.com>
2026-09-13 20:12:24 +0200

    update: add solution fix to issue two

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,30 +3,32 @@
 #include "binary_trees.h"
 
 /**
- * binary tree insert left - creates a binary tree function to insert left
- * @parent: pointer to the parent node of where we are to insert to
- * @value: value to be put in the left node
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer to the node to insert the left-child in
+ * @value: value to store in the new node
+ *
+ * Return: pointer to the created node, or NULL on failure or if
+ * parent is NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) 
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *temp;
-    
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
-    
-    temp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    if (temp == NULL)
-    {
-        printf("Memory allocation failed\n");
-        return (NULL);
-    }
-    temp->n = value;
-    temp->parent = parent;
-    temp->left = parent->left;
-    temp->right = NULL;
-    parent->left = temp;
+	binary_tree_t *temp;
 
-    return (temp);
+	if (parent == NULL)
+		return (NULL);
+
+	temp = malloc(sizeof(binary_tree_t));
+	if (temp == NULL)
+	{
+		printf("Memory allocation failed\n");
+		return (NULL);
+	}
+
+	temp->n = value;
+	temp->parent = parent;
+	temp->left = parent->left;
+	temp->right = NULL;
+	parent->left = temp;
+
+	return (temp);
 }
