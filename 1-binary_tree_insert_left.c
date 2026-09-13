bb699b2c32a9d4825e8cb7877677f3baa211c91f
Nkem Vincent Nweke <n.nweke@alustudent.com>
2026-09-13 20:26:44 +0200

    update: add solution fix to white space issue

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,10 +27,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	temp->n = value;
 	temp->parent = parent;
 	temp->left = parent->left;
-    if (temp->left != NULL)
-    {
-        temp->left->parent = temp;
-    }
+	if (temp->left != NULL)
+	{
+		temp->left->parent = temp;
+	}
 
 	temp->right = NULL;
 	parent->left = temp;
