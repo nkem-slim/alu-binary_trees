228d04e33e7bd4629291dacfe7ee5c84d1619aa8
Nkem Vincent Nweke <n.nweke@alustudent.com>
2026-09-13 20:22:43 +0200

    update: add solution fix to issue two

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,8 +28,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	temp->parent = parent;
 	temp->left = parent->left;
     if (temp->left != NULL)
+    {
         temp->left->parent = temp;
-        
+    }
+
 	temp->right = NULL;
 	parent->left = temp;
 
